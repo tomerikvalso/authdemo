c6ba5865ce285fdff28c8adc527bc012f215cffe
TEV013 <tomerikv@yahoo.com>
2026-04-24 14:32:03 +0200

    wewew

---
+++ src/main/java/com/tomerik/authdemo/HomeCOntroller.java
@@ -0,0 +1,19 @@
+package com.tomerik.authdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeCOntroller {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello world";
+    }
+
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello Secured";
+    }
+}
